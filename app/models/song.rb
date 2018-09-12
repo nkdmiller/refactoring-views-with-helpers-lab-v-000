16e37be79f6413554a9082cce0a0523bccb18d0e
Nate Miller <natekmiller96@gmail.com>
2018-09-12 23:20:09 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,11 +1,19 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
   def artist_name
-    self.artist.name
+    if self.artist
+      self.artist.name
+    end
   end
 
   def artist_name=(name)
-    @artist = artist.find(self.artist_id)
-    @artist.name = name
+    if self.artist
+      artist = Artist.find_by(name: self.artist.name)
+      artist.name
+    else 
+      artist = Artist.find_or_create_by(name: name)
+      self.artist = artist
+      self.artist.name
+    end
   end
 end
